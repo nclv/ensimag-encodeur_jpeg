2a1c15b880f34552c23d80033783b02a056a0da8
girodli <lisa.girod@grenoble-inp.org>
2020-05-12 15:32:18 +0200

    test pour la conversion

---
+++ src/test_conversion.c
@@ -0,0 +1,17 @@
+#include "../include/conversion.h"
+
+int main(void){
+
+  triplet_rgb* rgb = (triplet_rgb*) malloc(sizeof(triplet_rgb));;
+  rgb->triplet[0] = 255;
+	rgb->triplet[1] = 255;
+	rgb->triplet[2] = 255;
+
+  triplet_ycbcr* ycbcr = conversion(rgb);
+
+	printf("Affichage des pixels en RGB :\n");
+	printf("%d %d %d\n", rgb->triplet[R], rgb->triplet[G], rgb->triplet[B]);
+
+	printf("Affichage des pixels en YCbCr :\n");
+	printf("%f %f %f\n", ycbcr->triplet[0], ycbcr->triplet[1], ycbcr->triplet[2]);
+}
