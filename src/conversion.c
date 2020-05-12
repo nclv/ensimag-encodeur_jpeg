989b3dfef9307ffc6456626d2537740304b19953
girodli <lisa.girod@grenoble-inp.org>
2020-05-12 15:18:17 +0200

    retrait du main

--- src/conversion.c
+++ src/conversion.c
@@ -10,15 +10,3 @@ triplet_ycbcr* conversion(triplet_rgb* rgb) {
 
 	return ycbcr;
 }
-
-int main(void){
-  triplet_rgb* rgb = (triplet_rgb*) malloc(sizeof(triplet_rgb));
-  rgb->triplet[0] = 255;
-	rgb->triplet[1] = 255;
-	rgb->triplet[2] = 255;
-  triplet_ycbcr* ycbcr = conversion(rgb);
-	printf("Affichage des pixels en RGB :\n");
-	printf("%d %d %d\n", rgb->triplet[R], rgb->triplet[G], rgb->triplet[B]);
-	printf("Affichage des pixels en YCbCr :\n");
-	printf("%f %f %f\n", ycbcr->triplet[0], ycbcr->triplet[1], ycbcr->triplet[2]);
-}
