b364099473fbe2162082c4f360bf91013aa77668
n0way <n0way@localhost.localdomain>
2020-05-13 08:54:27 +0200

    Test compilation des fichiers .sh Changement du type de données Y Cb Cr en uint8_t

--- include/conversion.h
+++ include/conversion.h
@@ -1,11 +1,14 @@
 #ifndef _CONV_H
 #define _CONV_H
 
+#include <stdint.h>
+
 #include "../include/ppm.h"
 
 /* Définition et déclaration d'un triplet de couleurs YCbCr */
+/* Y is defined to have a range of 16 to 235; Cb and Cr are defined to have a range of 16 to 240*/
 typedef struct triplet_ycbcr {
-    float triplet[3];
+    uint8_t triplet[3];
 } triplet_ycbcr;
 
 /*
--- src/conversion.c
+++ src/conversion.c
@@ -5,9 +5,9 @@
 triplet_ycbcr* conversion(triplet_rgb* rgb) {
     triplet_ycbcr* ycbcr = (triplet_ycbcr*)malloc(sizeof(triplet_ycbcr));
 
-    ycbcr->triplet[0] = 0.299 * (float)rgb->triplet[R] + 0.587 * (float)rgb->triplet[G] + 0.114 * (float)rgb->triplet[B];
-    ycbcr->triplet[1] = -0.1687 * (float)rgb->triplet[R] - 0.3313 * (float)rgb->triplet[G] + 0.5000 * (float)rgb->triplet[B] + 128.0;
-    ycbcr->triplet[2] = 0.5000 * (float)rgb->triplet[R] - 0.4187 * (float)rgb->triplet[G] - 0.0813 * (float)rgb->triplet[B] + 128.0;
+    ycbcr->triplet[0] = 0.299 * rgb->triplet[R] + 0.587 * rgb->triplet[G] + 0.114 * rgb->triplet[B];
+    ycbcr->triplet[1] = -0.1687 * rgb->triplet[R] - 0.3313 * rgb->triplet[G] + 0.5000 * rgb->triplet[B] + 128.0;
+    ycbcr->triplet[2] = 0.5000 * rgb->triplet[R] - 0.4187 * rgb->triplet[G] - 0.0813 * rgb->triplet[B] + 128.0;
 
     return ycbcr;
 }
