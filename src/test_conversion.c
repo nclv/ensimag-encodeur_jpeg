360a150966c9ed84a75cfe6a110b05eeea3163bc
Florian Quillot <Florian.Quillot@grenoble-inp.org>
2020-05-13 11:52:10 +0200

    structure pixel modifée avec triplet rgb et triplet ycbcr, modification de la fonction de conversion pour etre adaptée à la nouvelle structure

--- src/test_conversion.c
+++ src/test_conversion.c
@@ -1,16 +1,18 @@
 #include "../include/conversion.h"
 
 int main(void) {
-	pixel_rgb* rgb = (pixel_rgb*)malloc(sizeof(pixel_rgb));
+	pixel* pixel = (pixel*)malloc(sizeof(pixel));
 
-    rgb->r = 255;
-    rgb->g = 255;
-    rgb->b = 255;
+    pixel->triplet[0] = 255;
+    pixel->triplet[1] = 255;
+		pixel->triplet[2] = 255;
 
-    pixel_ycbcr* ycbcr = conversion(rgb);
 
-    printf("Affichage des pixels en RGB :\n");
-    printf("%d %d %d\n", rgb->r, rgb->g, rgb->b);
+		printf("Affichage des pixels en RGB :\n");
+		printf("%f %f %f\n", ycbcr->y, ycbcr->cb, ycbcr->cr);
+
+		conversion(pixel);
+
 
     printf("Affichage des pixels en YCbCr :\n");
     printf("%f %f %f\n", ycbcr->y, ycbcr->cb, ycbcr->cr);
