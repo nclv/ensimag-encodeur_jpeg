649f37051f5e0bfa28ff37ad6caa8cb5ad594c67
n0way <n0way@localhost.localdomain>
2020-05-14 13:48:49 +0200

    Fonction bitstream_flush et correction de  l'écriture dans le bitstream

--- src/bitstream.c
+++ src/bitstream.c
@@ -4,14 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define DEFAULT_BUFFER_SIZE 512
+#define DEFAULT_BUFFER_SIZE 1
 
 bitstream *bitstream_create(const char *filename) {
     bitstream *stream = malloc(sizeof *stream);
     if (stream == NULL) return NULL;
 
     stream->buffer_size = DEFAULT_BUFFER_SIZE;
-    stream->buffer = malloc((sizeof(unsigned char *) + 1) * DEFAULT_BUFFER_SIZE);  // sizeof(unsigned char *) + 1
+    stream->buffer = malloc((sizeof(unsigned char *) + 1) * DEFAULT_BUFFER_SIZE);
     /* printf("%ld", sizeof stream->buffer); */
     /* Renvoie 8 * DEFAULT_BUFFER_SIZE */
     if (stream->buffer == NULL) {
@@ -26,6 +26,8 @@ bitstream *bitstream_create(const char *filename) {
 }
 
 void bitstream_display(bitstream *stream) {
+    assert(stream != NULL);
+
     printf("Filename: %s\n", stream->filename);
     printf("Buffer size: %ld\n", stream->buffer_size * 8);
     printf("Nombre de bits dans le buffer: %ld\n", stream->last_written_bit_offset);
@@ -37,28 +39,40 @@ void bitstream_display(bitstream *stream) {
     printf("\n");
 }
 
-void bitstream_flush(bitstream *stream);
+void bitstream_flush(bitstream *stream) {
+    size_t length = stream->last_written_bit_offset;
+    for (uint32_t i = 0; i < length; i++) {
+        printf("Ecriture du bit %d dans le fichier jpeg\n", stream->buffer[i]);
+        stream->buffer[i] = 0;
+        stream->last_written_bit_offset--;
+    }
+}
 
 void bitstream_write_bits(bitstream *stream, uint32_t value, uint8_t nb_bits, bool is_marker) {
     assert(0 < nb_bits && nb_bits <= 32);
 
     printf("Marqueur: %i\n", is_marker);
 
+    if (DEFAULT_BUFFER_SIZE * 8 - stream->last_written_bit_offset < nb_bits) {
+        printf("Dépassement de la capacité du buffer\n");
+        bitstream_display(stream);
+        bitstream_flush(stream);
+    }
+
     uint32_t mask = 1U << (nb_bits - 1);
 
+    size_t current_bit_offset = stream->last_written_bit_offset;
     for (uint32_t i = 0; i < nb_bits; i++) {
         /* Dépassement de la capacité du buffer, pas d'erreur visible sauf sur valgrind */
-        if (stream->last_written_bit_offset + i > DEFAULT_BUFFER_SIZE * 8) {
-            printf("Dépassement de la capacité du buffer\n");
-            // bitstream_flush(stream);
-        }
-        stream->buffer[stream->last_written_bit_offset + i] = (value & mask) ? 1 : 0;
+        stream->buffer[current_bit_offset + i] = (value & mask) ? 1 : 0;
         value <<= 1;
+        stream->last_written_bit_offset += 1;
     }
-    stream->last_written_bit_offset += (size_t)(nb_bits - 1);
 }
 
 void bitstream_destroy(bitstream *stream) {
-    free(stream->buffer);
-    free(stream);
+    if (stream != NULL) {
+        free(stream->buffer);
+        free(stream);
+    }
 }
