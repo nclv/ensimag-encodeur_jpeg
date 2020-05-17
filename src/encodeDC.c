bd5c50a8e98d5efb728c6f62fdd6291c903f3216
Florian Quillot <Florian.Quillot@grenoble-inp.org>
2020-05-17 12:30:41 +0200

    encodeDC classe de magnitude ok

--- src/encodeDC.c
+++ src/encodeDC.c
@@ -1,7 +1,6 @@
 #include "encodeDC.h"
 #include <math.h>
 #include <stdint.h>
-#include <string.h>
 
 uint32_t encoderDC(uint8_t MCU_courante_DC, uint8_t MCU_ancienne_DC) {
   uint8_t valeur_a_encoder = MCU_courante_DC - MCU_ancienne_DC;
@@ -10,6 +9,6 @@ uint32_t encoderDC(uint8_t MCU_courante_DC, uint8_t MCU_ancienne_DC) {
     valeur_a_encoder = -valeur_a_encoder;
     est_negatif = 1;
   }
-  uint8_t classe_magnitude = ((uint8_t)(((string)log2(valeur_a_encoder))[0]))+1;
-  
+  uint8_t classe_magnitude = ((uint8_t)log2(valeur_a_encoder))+1;
+
 }
