55835ac6a6a11d6ad1c56b8b8ae4171c5085eb65
Florian Quillot <Florian.Quillot@grenoble-inp.org>
2020-05-26 12:24:36 +0200

    pseudocode modified

--- src/not_to_compile/pseudocode.c
+++ src/not_to_compile/pseudocode.c
@@ -1,21 +1,24 @@
 // pseudo code à la va très vite que je reprendrai demain mais pour l'idée générale
 
 void ecriture_dans_le_bitstream(bitstream, huff1, huff2, bloc, difference_DC) {
-    uint nb_16_zeros;
-    uint nb_zeros_restants;
     uint nb_zeros_enchaines = 0;
-    uint magnitude;
-    uint indice;
     for k in range 8 {
         for l in range 8 {
             if (k == 0 and l == 0) {
-                magnitude, indice  =  encodeDC(difference_DC);
-                ecrire_bitstream(magnitude +  indice);
+                // ecrit dans le bitstream magnitude + indice
+                encodeDC(difference_DC);
             }
             else {
-                magnitude,  indice, nb_16_zeros, nb_zeros_restants =encodeAC(matrice[k][l], 16*nb_16_zeros+nb_zeros_restants);
-                if (16*nb_16_zeros+nb_zeros_restants != 0) {
-                    ecrire_bitstream(nb_16_zeros*ZRL + nb_zeros_restants + magnitude + indice);
+                if (matrice[k][l] == 0) {
+                    // incrementer les zeros a la suite
+                    nb_zeros_enchaines++;
+                }
+                else {
+                    /* ecrit dans le bitstream le bon nb de ZRL, le nb de zeros restants,
+                       la magnitude et l'indice */
+                    encodeAC(matrice[k][l], nb_zeros_enchaines);
+                    // reset le compteur de zeros  enchaines
+                    nb_zeros_enchaines = 0;
                 }
             }
     ecrire_bitstream(EOB);
