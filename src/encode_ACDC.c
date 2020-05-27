3082f23022432c97942e984df7f85fd6e1821fd8
n0way <n0way@localhost.localdomain>
2020-05-27 11:35:38 +0200

    Valgrind invader

--- src/encode_ACDC.c
+++ src/encode_ACDC.c
@@ -75,9 +75,6 @@ static void encoder_AC_freq(bitstream *stream, huff_table *ac_table, int16_t fre
     uint32_t code_magnitude = huffman_table_get_path(ac_table, (uint8_t)value, &nb_bits_zeros_magnitude);
     bitstream_write_bits(stream, code_magnitude, nb_bits_zeros_magnitude, false);
     bitstream_write_bits(stream, indice, classe_magnitude, false);
-
-    printf("coef : %i\n", freq_AC);
-    printf("classe : %i\nindice : %i\n", classe_magnitude, indice);
 }
 
 void ecrire_coeffs(bitstream *stream, int16_t data_unit[8][8], huff_table *dc_table, huff_table *ac_table, int16_t difference_DC) {
