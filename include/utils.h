2e8c3ed0f6956218edfe7b5ce19e13c042778f36
Florian Quillot <Florian.Quillot@grenoble-inp.org>
2020-05-27 10:50:51 +0200

    utils.h doc

--- include/utils.h
+++ include/utils.h
@@ -1,8 +1,17 @@
 #ifndef _UTILS_H_
 #define _UTILS_H_
 
+/* type : const char*
+ * rtype : char*
+ * Permet de duppliquer une chaîne de caractères
+ */
 extern char *strdup(const char *str);
 
+/* type : char*
+ * rtype : void
+ * Enlève l'extension d'une chaîne de caractères
+ * représentant un nom de fichier
+ */
 extern void strip_ext(char *fname);
 
 #endif
