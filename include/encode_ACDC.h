2d6993927f5ecc408fb88b6a49a288eba31a1fa2
Florian Quillot <Florian.Quillot@grenoble-inp.org>
2020-05-27 10:27:58 +0200

    docs encodeACDC

--- include/encode_ACDC.h
+++ include/encode_ACDC.h
@@ -6,6 +6,11 @@
 #include "bitstream.h"
 #include "huffman.h"
 
+/* type: bitstream*, int16_t[8][8], huff_table*, huff_table*, int16_t
+ * rtype: void
+ * Encode chaque coefficient de la data unit 8x8 DC et AC avec les codes RLE
+ * et les tables de huffman et écrit les symboles dans le bitstream
+ */
 extern void ecrire_coeffs(bitstream *stream, int16_t data_unit[8][8], huff_table *dc_table, huff_table *ac_table, int16_t difference_DC);
 
 #endif
